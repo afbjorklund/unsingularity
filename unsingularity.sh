420982737cfb1f1a54eb6a1794267c9dac6f0a0e
Anders F Björklund <anders.f.bjorklund@gmail.com>
2023-03-01 22:23:48 +0100

    Look for -o option
    Some decade old versions do not support the offset parameter.
    
    Use workaround by dumping to a temporary files, for those (el7)

--- unsingularity.sh
+++ unsingularity.sh
@@ -23,10 +23,23 @@ do
 done
 shift $((OPTIND - 1))
 
+if unsquashfs -h 2>&1 | grep -- -o >/dev/null; then
+	has_offset=true
+else
+	has_offset=false
+fi
+
 for sif in "$@"; do
 	if $cat; then directory=$(mktemp -d); rmdir "$directory"; noprogress="-n"; tail="+10"; fi
-	offset=$(apptainer sif list "$sif" | grep Squashfs | cut -d'|' -f4 | cut -d'-' -f1)
-	unsquashfs $noprogress -o $offset $ls -d $directory -e $extract $sif | tail -n $tail
+	if $hasoffset; then
+		offset=$(apptainer sif list "$sif" | grep Squashfs | cut -d'|' -f4 | cut -d'-' -f1)
+		unsquashfs $noprogress -o $offset $ls -d $directory -e $extract $sif | tail -n $tail
+	else
+		layer=$(apptainer sif list "$sif" | grep Squashfs | cut -d'|' -f1)
+		apptainer sif dump $layer "$sif" > "$sif.squashfs"
+		unsquashfs $noprogress $ls -d $directory -e $extract $sif.squashfs | tail -n $tail
+		rm "$sif.squashfs"
+	fi
 	if $cat; then xargs -I% cat $directory/% <$extract; rm -rf "$directory"; fi
 done
 rm $extract || exit 1
